1a99528688d8f10178321de0341687b786d4b105
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-12-28 23:16:51 +0200

    upd ResultDisplay.jsx

--- src/components/ResultDisplay/ResultDisplay.jsx
+++ src/components/ResultDisplay/ResultDisplay.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import "./ResultDisplay.css";  // Підключаємо стилі для пазлів
+import "./ResultDisplay.css";  
 
 const ResultDisplay = ({ result }) => {
-  if (result.length === 0) return null;
-  
+  if (!result || result.length === 0) return null;
+  const getRandomColor = () => {
+    const randomHex = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${randomHex.padStart(6, "0")}`;
+  };
+    
   return (
     <div className="puzzle-container">
       {result.map((puzzlePiece, index) => (
-        <div key={index} className="puzzle-piece">
+        <div key={index} className="puzzle-piece" style={{ backgroundColor: getRandomColor(puzzlePiece) }}>
           {puzzlePiece}
         </div>
       ))}
