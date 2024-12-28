201b6efb715df117aa6e0b94b760b89b7879e517
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2024-12-28 23:16:02 +0200

    upd NumberPuzzle

--- src/components/NumberPuzzle/NumberPuzzle.jsx
+++ src/components/NumberPuzzle/NumberPuzzle.jsx
@@ -1,91 +1,72 @@
-
-import React, { useState} from "react";
-import FileUploader from "../FileUploader";
+import React, { useState, useRef} from "react";
+import FileUploader from "../FileUploader/FileUploader";
 import ResultDisplay from "../ResultDisplay/ResultDisplay";
 import findSequence from "../../utils/findSequence";
+import { PulseLoader } from "react-spinners";
+import './NumberPuzzle.css';
+
 
 const NumberPuzzle = () => {
   const [numbers, setNumbers] = useState([]);
   const [result, setResult] = useState([]);
   const [sequenceString, setSequenceString] = useState(""); // Для рядка результату
-  const handleCalculate = () => {
-    if (numbers.length === 0) {
+  const [loading, setLoading] = useState(false); 
+  const fileInputRef = useRef(null); 
+
+  const handleCalculate = async () => {
+    if (!numbers || numbers.length === 0) {
       alert("Спочатку завантажте файл із числами.");
       return;
     }
-    const { puzzlePieces, result } = findSequence(numbers); // Отримуємо обидва значення
 
-    // Оновлюємо стан результату
-    setResult(puzzlePieces);
-    setSequenceString(result); // Оновлюємо рядок
+    setLoading(true);
+
+    // Емуляція асинхронного обчислення
+    setTimeout(() => {
+      const { puzzlePieces, result } = findSequence(numbers);
+
+      // Оновлюємо стан результату
+      setResult(puzzlePieces);
+      setSequenceString(result); // Оновлюємо рядок
+
+      setLoading(false); // Вимкнути лоадер
+      if (fileInputRef.current) {
+        fileInputRef.current.value = null;
+      }
+    }, 1000); // Емуляція затримки (1 секунда)
   };
   
+  
   return (
-    <div className="puzzle">
-      <h1>Цифровий Пазл</h1>
-      <FileUploader onFileUpload={setNumbers} />
-      <button onClick={handleCalculate}>Знайти Послідовність</button>
-      <ResultDisplay result={result}/> 
-      {sequenceString && (
-        <div>
-          <h2>З'єднаний Рядок:</h2>
-          <p>{sequenceString}</p> {/* Виведення з'єднаного рядка */}
-        </div>
-      )}
+    <div >
+      <h1 className="puzzle-title">Цифровий Пазл</h1>
+      <div className="puzzle-header">
+        <FileUploader onFileUpload={setNumbers} inputRef={fileInputRef}/>
+        <button onClick={handleCalculate}>Знайти Послідовність</button>
+      </div>
+     
+      <div className="puzzle-container">
+        {loading ? (
+          <div className="loader">
+            <PulseLoader color="#0909db" size={15} />
+            <p className="puzzle-loader">Йде пошук співпадінь, зачекайте...</p>
+          </div>
+        ) : (
+          <>
+            <ResultDisplay result={result} />
+            {sequenceString && (
+              <div className="number-container">
+                <h2 className="puzzle-title">З'єднаний Рядок:</h2>
+                <p className="number-sequence">{sequenceString}</p> {/* Виведення з'єднаного рядка */}
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
 
 export default NumberPuzzle;
-
-
-// import React, { useState, useRef, useEffect} from "react";
-// import FileUploader from "../FileUploader";
-// import ResultDisplay from "../ResultDisplay/ResultDisplay";
-
-
-// const NumberPuzzle = () => {
-//   const [numbers, setNumbers] = useState([]);
-//   const [result, setResult] = useState([]);
-//   const workerRef = useRef(null);
-
-//   useEffect(() => {
-//     // Створюємо Web Worker з відносного шляху до public/workers/worker.js
-//     workerRef.current = new Worker("/workers/worker.js");
-
-//     // Обробка повідомлень від Worker
-//     workerRef.current.onmessage = (e) => {
-//       setResult(e.data); // Встановлюємо результат від Worker
-//     };
-
-//     // Завершення роботи Worker при демонтажі компонента
-//     return () => {
-//       workerRef.current.terminate();
-//     };
-//   }, []);
-
-//   const handleCalculate = () => {
-//     if (numbers.length === 0) {
-//       alert("Спочатку завантажте файл із числами.");
-//       return;
-//     }
-
-//     // Надсилаємо числа в Worker для обчислень
-//     workerRef.current.postMessage(numbers);
-//   };
-
-
-//   return (
-//     <div>
-//       <h1>Цифровий Пазл</h1>
-//       <FileUploader onFileUpload={setNumbers} />
-//       <button onClick={handleCalculate}>Знайти Послідовність</button>
-//       <ResultDisplay result={result} />
-//     </div>
-//   );
-// };
-
-// export default NumberPuzzle;
-
 
 
